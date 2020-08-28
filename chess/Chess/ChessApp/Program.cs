b032c092266cddc4af3b3f1bd2e531e97c4d6f71
antonthunmanjonsson <jonssonanton@hotmail.com>
2020-08-28 15:20:21 +0200

    Chess app

---
+++ chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("░");
+            //Console.Write("▓");
+
+           
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 16; j++)
+                {
+                    int total = i + (j/2);
+                    int oddOrEven = total % 2;
+
+                    if (oddOrEven == 0)
+                    {
+                        Console.Write("▓");
+                    }
+                    else
+                    {
+                        Console.Write("░");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ chess/Chess/flag/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace flag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("░");
+            //Console.Write("▓");
+
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 20; col++)
+                {
+                    Console.Write("j");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
