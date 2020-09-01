3bdd883614cab869d804ec9f2de13c8873f4f0a8
antonthunmanjonsson <jonssonanton@hotmail.com>
2020-09-01 09:43:43 +0200

    ChessApp

---
+++ chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Rita ett schackbräde med hjälp av dessa två tecken ░ ▓.
+            // Använd gärna metoder för att lösa problemet.
+            // Man behöver använda % (modulo)
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("▓");
+                    }
+                    else
+                    {
+                        Console.Write("░");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    for (int j = 0; j < 16; j++)
+            //    {
+            //        int total = i + (j / 2);
+            //        int oddoreven = total % 2;
+
+            //        if (oddoreven == 0)
+            //        {
+            //            Console.write("▓");
+            //        }
+            //        else
+            //        {
+            //            Console.write("░");
+            //        }
+            //    }
+
+            //    Console.writeline();
+            //}
+            Console.ReadKey();
+        }
+    }
+}
