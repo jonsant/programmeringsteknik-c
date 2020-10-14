b27dda79e8ce3d7176d36bedbf1b90119583e5e2
antonthunmanjonsson <jonssonanton@hotmail.com>
2020-10-14 08:38:44 +0200

    brownfield & friday

---
+++ brownfield/TimeSheet.Common/Data/CustomerLibrary.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+using TimeSheet.Common.Models;
+
+namespace TimeSheet.Common.Data
+{
+    public class CustomerLibrary
+    {
+        public static List<CustomerModel> GetCustomers()
+        {
+            return new List<CustomerModel>
+            {
+                new CustomerModel() { Name = "ABC", HourlyRate = 150 },
+                new CustomerModel() { Name = "Acme", HourlyRate = 125 }
+            };
+        }
+    }
+}
--- friday/tgif/Program.cs
+++ friday/tgif/Program.cs
@@ -7,7 +7,39 @@ namespace tgif
         static void Main(string[] args)
         {
             // Skriv en applikation som läser in ett datum via användarinmatning,
-            // som sedan räknar ut hur många dagar det är till nästa fredag.
+            // som sedan räknar ut hur många dagar det är till nästkommande fredag
+            // från det inmatade datumet
+            // måndag är den första dagen i veckan
+
+            Console.WriteLine("Skriv ett datum: ");
+            //string input = Console.ReadLine();
+
+            if (DateTime.TryParse(Console.ReadLine(), out DateTime dateResult))
+            {
+                int daysCounter = 0;
+
+                while (dateResult.DayOfWeek != DayOfWeek.Friday)
+                {
+                    dateResult = dateResult.AddDays(1);
+                    daysCounter++;
+                }
+
+                if (daysCounter == 0)
+                {
+                    Console.WriteLine("Fredag idag!");
+                }
+                else
+                {
+                    Console.WriteLine($"{daysCounter} {(daysCounter > 1 ? "dagar" : "dag")} kvar till fredag!");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Ej datum?");
+            }
+            
+
         }
     }
 }
